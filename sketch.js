836f70f48537d4d785e35b86f19a9ab7698052df
kemilyrodrigues <147505911+kemilyrodrigues@users.noreply.github.com>
2023-10-17 08:07:37 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+etup() {
+    createCanvas(400, 400);
+    background("pink");
+  }
+  
+  function draw() {
+    stroke("blue")
+    fill("purple")
+    
+    if(mouseIsPressed) {
+    rect(mouseX,mouseY,20,20);
+  }
+  }
